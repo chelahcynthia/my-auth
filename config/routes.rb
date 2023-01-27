ac941f56d6293c01afd4b6a1723449d6f2e43065
cynthia <cchelangat9892gmail.com>
2023-01-27 20:56:53 +0300

    Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts
-  get '/register' , to: 'users#create'
+  get 'users/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
