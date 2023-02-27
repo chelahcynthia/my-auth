a1459395be7a2b24be824a4ce9e33eaab3607c94
cynthia <cchelangat9892gmail.com>
2023-02-27 22:32:38 +0300

    added routes to register a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-Rails.application.routes.draw do
-  get 'users/new'
+Rails.application.routes.draw do  
+resource :users, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
